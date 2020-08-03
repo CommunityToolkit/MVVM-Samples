91c33264c7cd22847829cb72f55cd48c5fe1e8a1
Sergio Pedri <sergio0694@live.com>
2020-08-03 22:29:56 +0200

    Improved template for code samples

--- samples/MvvmSampleUwp/MvvmSampleUwp/Controls/InteractiveSample.cs
+++ samples/MvvmSampleUwp/MvvmSampleUwp/Controls/InteractiveSample.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace MvvmSampleUwp.Controls
 {
@@ -7,5 +8,40 @@ namespace MvvmSampleUwp.Controls
     /// </summary>
     public sealed class InteractiveSample : ContentControl
     {
+        /// <summary>
+        /// Gets or sets the <see cref="string"/> representing the C# code to display.
+        /// </summary>
+        public string CSharpCode
+        {
+            get => (string)GetValue(CSharpCodeProperty);
+            set => SetValue(CSharpCodeProperty, $"```csharp\n{value.Trim()}\n```");
+        }
+
+        /// <summary>
+        /// The <see cref="DependencyProperty"/> backing <see cref="CSharpCode"/>.
+        /// </summary>
+        public static readonly DependencyProperty CSharpCodeProperty = DependencyProperty.Register(
+            nameof(CSharpCode),
+            typeof(string),
+            typeof(InteractiveSample),
+            new PropertyMetadata(default(string)));
+
+        /// <summary>
+        /// Gets or sets the <see cref="string"/> representing the XAML code to display.
+        /// </summary>
+        public string XamlCode
+        {
+            get => (string)GetValue(XamlCodeProperty);
+            set => SetValue(XamlCodeProperty, $"```xml\n{value.Trim()}\n```");
+        }
+
+        /// <summary>
+        /// The <see cref="DependencyProperty"/> backing <see cref="CSharpCode"/>.
+        /// </summary>
+        public static readonly DependencyProperty XamlCodeProperty = DependencyProperty.Register(
+            nameof(XamlCode),
+            typeof(string),
+            typeof(InteractiveSample),
+            new PropertyMetadata(default(string)));
     }
 }
