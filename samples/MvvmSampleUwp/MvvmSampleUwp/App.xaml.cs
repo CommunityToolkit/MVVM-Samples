fdaf9bf08363de80dcc53933c9fdaf793948a94e
Sergio Pedri <sergio0694@live.com>
2020-08-03 14:50:01 +0200

    Initial project setup

---
+++ samples/MvvmSampleUwp/MvvmSampleUwp/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+
+namespace MvvmSampleUwp
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default <see cref="Application"/> class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object. This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            // Ensure the UI is initialized
+            Window.Current.Content ??= new Shell();
+
+            // Enable the prelaunch if needed, and activate the window
+            if (e.PrelaunchActivated == false)
+            {
+                CoreApplication.EnablePrelaunch(true);
+
+                Window.Current.Activate();
+            }
+        }
+    }
+}
---
+++ samples/MvvmSampleUwp/MvvmSampleUwp/Shell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MvvmSampleUwp
+{
+    public sealed partial class Shell : UserControl
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
