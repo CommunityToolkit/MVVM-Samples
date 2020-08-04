c442923537167190eb6a3935ec16d6fa410c98b7
Sergio Pedri <sergio0694@live.com>
2020-08-04 15:17:34 +0200

    Added AsyncRelayCommand<T> docs page

---
+++ samples/MvvmSampleUwp/MvvmSampleUwp/ViewModels/AsyncRelayCommandPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace MvvmSampleUwp.ViewModels
+{
+    public class AsyncRelayCommandPageViewModel : SamplePageViewModel
+    {
+        public AsyncRelayCommandPageViewModel()
+        {
+            DownloadTextCommand = new AsyncRelayCommand(DownloadTextAsync);
+        }
+
+        public IAsyncRelayCommand DownloadTextCommand { get; }
+
+        private async Task<string> DownloadTextAsync()
+        {
+            await Task.Delay(3000); // Simulate a web request
+
+            return "Hello world!";
+        }
+    }
+}
