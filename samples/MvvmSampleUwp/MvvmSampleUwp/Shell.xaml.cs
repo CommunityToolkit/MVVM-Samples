9e6f422e6e55f18d2f83b9a35fecc92c3a7ece1a
Sergio Pedri <sergio0694@live.com>
2020-08-04 19:38:49 +0200

    Improved navigation system

--- samples/MvvmSampleUwp/MvvmSampleUwp/Shell.xaml.cs
+++ samples/MvvmSampleUwp/MvvmSampleUwp/Shell.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using MvvmSampleUwp.Views;
 using NavigationView = Microsoft.UI.Xaml.Controls.NavigationView;
+using NavigationViewItem = Microsoft.UI.Xaml.Controls.NavigationViewItem;
 using NavigationViewBackRequestedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs;
 using NavigationViewItemInvokedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs;
 
@@ -11,9 +14,23 @@ namespace MvvmSampleUwp
 {
     public sealed partial class Shell : UserControl
     {
+        private readonly IReadOnlyCollection<(NavigationViewItem Item, Type PageType)> NavigationItems;
+
         public Shell()
         {
             this.InitializeComponent();
+
+            NavigationItems = new[]
+            {
+                (IntroductionItem, typeof(IntroductionPage)),
+                (ObservableObjectItem, typeof(ObservableObjectPage)),
+                (CommandsItem, typeof(RelayCommandPage)),
+                (AsyncCommandsItem, typeof(AsyncRelayCommandPage)),
+                (MessengerItem, typeof(MessengerPage)),
+                (SendMessagesItem, typeof(MessengerSendPage)),
+                (RequestMessagesItem, typeof(MessengerRequestPage)),
+                (InversionOfControlItem, typeof(IocPage))
+            };
 
             // Set the custom title bar to act as a draggable region
             Window.Current.SetTitleBar(TitleBarBorder);
@@ -22,10 +39,8 @@ namespace MvvmSampleUwp
         // Navigates to a sample page when a button is clicked
         private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
-            if (args.InvokedItemContainer.Tag is string pageTypeName)
+            if (NavigationItems.FirstOrDefault(item => item.Item == args.InvokedItemContainer).PageType is Type pageType)
             {
-                Type pageType = Type.GetType($"MvvmSampleUwp.Views.{pageTypeName}");
-
                 NavigationFrame.Navigate(pageType);
             }
         }
@@ -39,7 +54,12 @@ namespace MvvmSampleUwp
         // Navigates back
         private void NavigationView_OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
         {
-            NavigationFrame.GoBack();
+            if (NavigationFrame.BackStack.LastOrDefault() is PageStackEntry entry)
+            {
+                NavigationView.SelectedItem = NavigationItems.First(item => item.PageType == entry.SourcePageType).Item;
+
+                NavigationFrame.GoBack();
+            }
         }
 
         // Select the introduction item when the shell is loaded
