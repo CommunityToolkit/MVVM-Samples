1dd0d8cd2837be562801da850f6ca181cef40bbc
Nick Randolph <nick@builttoroam.com>
2020-08-06 13:12:08 +1000

    Adding uno

---
+++ samples/MvvmSample/MvvmSample/MvvmSample.Shared/Shell.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using MvvmSampleUwp.Views;
+
+#nullable enable
+
+namespace MvvmSampleUwp
+{
+    public sealed partial class Shell : UserControl
+    {
+        private readonly IReadOnlyCollection<SampleEntry> NavigationItems;
+
+        public Shell()
+        {
+            this.InitializeComponent();
+
+            NavigationItems = new[]
+            {
+                new SampleEntry(IntroductionItem, typeof(IntroductionPage)),
+                new SampleEntry(ObservableObjectItem, typeof(ObservableObjectPage), "ObservableObject", "observable inotify property changed propertychanging changing"),
+                new SampleEntry(CommandsItem, typeof(RelayCommandPage), "RelayCommand and RelayCommand<T>", "commands icommand relaycommand binding"),
+                new SampleEntry(AsyncCommandsItem, typeof(AsyncRelayCommandPage), "AsyncRelayCommand and AsyncRelayCommand<T>", "asynccommands icommand relaycommand binding asynchronous"),
+                new SampleEntry(MessengerItem, typeof(MessengerPage), "Messenger and IMessenger", "messenger messaging message receiver recipient"),
+                new SampleEntry(SendMessagesItem, typeof(MessengerSendPage), "[IMessenger] Send messages", "messenger messaging message receiver recipient send"),
+                new SampleEntry(RequestMessagesItem, typeof(MessengerRequestPage), "[IMessenger] Request messages", "messenger messaging message receiver recipient request reply"),
+                new SampleEntry(InversionOfControlItem, typeof(IocPage), "Ioc (Inversion of control)", "ioc inversion control dependency injection service locator")
+            };
+
+            // Set the custom title bar to act as a draggable region
+            Window.Current.SetTitleBar(TitleBarBorder);
+        }
+
+        // Navigates to a sample page when a button is clicked
+        private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (NavigationItems.FirstOrDefault(item => item.Item == args.InvokedItemContainer)?.PageType is Type pageType)
+            {
+                NavigationFrame.Navigate(pageType);
+            }
+        }
+
+        // Sets whether or not the back button is enabled
+        private void NavigationFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            NavigationView.IsBackEnabled = ((Frame)sender).BackStackDepth > 0;
+        }
+
+        // Navigates back
+        private void NavigationView_OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (NavigationFrame.BackStack.LastOrDefault() is PageStackEntry entry)
+            {
+                NavigationView.SelectedItem = NavigationItems.First(item => item.PageType == entry.SourcePageType).Item;
+
+                NavigationFrame.GoBack();
+            }
+        }
+
+        // Select the introduction item when the shell is loaded
+        private void Shell_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            NavigationView.SelectedItem = IntroductionItem;
+
+            NavigationFrame.Navigate(typeof(IntroductionPage));
+        }
+
+        // Updates the search results
+        private void SearchBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                // Not a simple tokenized search, but good enough for now
+                string query = sender.Text.ToLowerInvariant();
+
+                sender.ItemsSource = NavigationItems.Where(item => item.Tags?.Contains(query) == true);
+            }
+        }
+
+        // Navigates to a selected item
+        private void AutoSuggestBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            SampleEntry entry = (SampleEntry)args.SelectedItem;
+
+            NavigationFrame.Navigate(entry.PageType);
+
+            NavigationView.SelectedItem = entry.Item;
+
+            sender.Text = string.Empty;
+        }
+    }
+
+    /// <summary>
+    /// A simple model for tracking sample pages associated with buttons.
+    /// </summary>
+    public sealed class SampleEntry
+    {
+        public SampleEntry(NavigationViewItem viewItem, Type pageType, string? name = null, string? tags = null)
+        {
+            Item = viewItem;
+            PageType = pageType;
+            Name = name;
+            Tags = tags;
+        }
+
+        /// <summary>
+        /// The navigation item for the current entry.
+        /// </summary>
+        public NavigationViewItem Item { get; }
+
+        /// <summary>
+        /// The associated page type for the current entry.
+        /// </summary>
+        public Type PageType { get; }
+
+        /// <summary>
+        /// Gets the name of the current entry.
+        /// </summary>
+        public string? Name { get; }
+
+        /// <summary>
+        /// Gets the tag for the current entry, if any.
+        /// </summary>
+        public string? Tags { get; }
+    }
+}
---
+++ samples/MvvmSample/MvvmSample/MvvmSample.Shared/Views/AsyncRelayCommandPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace MvvmSampleUwp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AsyncRelayCommandPage : Page
+    {
+        public AsyncRelayCommandPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+
+    // TODO: replace this with the one from the toolkit, when https://github.com/windows-toolkit/WindowsCommunityToolkit/pull/3410 is merged
+    public sealed class TaskResultConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Task task &&
+                task.IsCompletedSuccessfully)
+            {
+                return task.GetType().GetProperty(nameof(Task<object>.Result))?.GetValue(task);
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
