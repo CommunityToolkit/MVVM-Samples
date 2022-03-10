fed6414daad2d5a4f6c4f54ff56c78b0a0bbbb55
Sergio Pedri <sergio0694@live.com>
2022-03-10 14:09:09 +0100

    UI improvements to the docs sections

---
+++ samples/MvvmSampleUwp/Controls/DocumentationBlock.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MvvmSampleUwp.Controls
+{
+    /// <summary>
+    /// A simple control that acts as a container for a documentation block.
+    /// </summary>
+    public sealed class DocumentationBlock : ContentControl
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="string"/> representing the text to display.
+        /// </summary>
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        /// <summary>
+        /// The <see cref="DependencyProperty"/> backing <see cref="Text"/>.
+        /// </summary>
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(DocumentationBlock),
+            new PropertyMetadata(default(string)));
+    }
+}
